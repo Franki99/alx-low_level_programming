74c537579f059005497b2262a17b4b844e83b002
Franki99 <ntaganiradivanni@gmail.com>
2022-04-28 16:09:08 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,16 @@
 
 /**
  * _puts_recursion - prints a string
- * @s: string
+ * @s: strig
  * Return: no return
  */
-
 void _puts_recursion(char *s)
 {
-if (*s == 0)
+if (*s != '\0')
 {
-_putchar('\n');
-return;
-}
 _putchar(*s);
 _puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
